f8235fee19bf6192867c121224ed51b380320c12
Thomas Quintana <quintana.thomas@gmail.com>
2012-05-30 02:50:12 -0400

    Added missing author and license.

--- src/test/java/org/mobicents/commons/util/RevolvingCounterTest.java
+++ src/test/java/org/mobicents/commons/util/RevolvingCounterTest.java
@@ -1,3 +1,19 @@
+/*
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.mobicents.commons.util;
 
 import java.util.ArrayList;
@@ -15,6 +31,9 @@ import org.mobicents.commons.annotations.NotThreadSafe;
 import org.mobicents.commons.annotations.ThreadSafe;
 import org.mobicents.commons.util.concurrent.RevolvingCounter;
 
+/**
+ * @author quintana.thomas@gmail.com (Thomas Quintana)
+ */
 @ThreadSafe public final class RevolvingCounterTest {
   public RevolvingCounterTest() {
     super();
